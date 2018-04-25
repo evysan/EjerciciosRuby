4bb2da102b9fbf1340691dc2c31d8236bde54b87
Evy Andrades <evyandrade13@gmail.com>
2018-04-24 22:12:35 -0500

    Added Methods to Exercise3 and Added New Exercises

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,8 +1,15 @@
-puts 'ingrese un numero'
-numero1 = gets.chomp
-
-if numero1.to_i % 2 == 0
-  puts 'el numero es par'
-else
-  puts 'el numero es impar'
+def es_par? (numero1)
+  numero1 % 2 == 0
 end
+
+def respuesta (numero1)
+  if es_par? (numero1)
+    puts 'el numero es par'
+  else
+    puts 'el numero es impar'
+  end
+end
+
+puts 'ingrese un numero'
+numero1 = gets.chomp.to_i
+respuesta(numero1)
